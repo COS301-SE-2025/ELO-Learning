d56b32758fcd1c634b01cf4b0e644702a6c7e8c7
Saskia Steyn <u17267162@tuks.co.za>
2025-05-26 15:45:26 +0200

    finally fixed the stupid nav-bar

--- frontend/src/app/ui/nav-bar.jsx
+++ frontend/src/app/ui/nav-bar.jsx
@@ -3,7 +3,7 @@ export default function NavBar() {
   return (
     <div
       className="fixed bottom-0 left-0 w-full z-50 flex flex-col px-3 py-4
-                    md:static md:h-full md:w-auto md:px-2"
+                    md:static md:w-auto md:px-2"
     >
       <div
         className="flex grow flex-row justify-between space-x-2
--- frontend/src/app/ui/nav-links.jsx
+++ frontend/src/app/ui/nav-links.jsx
@@ -25,7 +25,7 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3',
+              'nav-link-item flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-[#e8e8e8] dark:bg-[#7d32ce] text-white':
                   pathname === link.href,

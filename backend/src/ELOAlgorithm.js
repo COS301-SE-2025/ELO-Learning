b629a0f6673bf9dae08ea8fd15941a081e78b293
Ntokozo <141838004+Ntokozo254@users.noreply.github.com>
2025-06-12 16:40:41 +0200

    63-backend-start-implementing-the-elo-algorithm

---
+++ backend/src/ELOAlgorithm.js
@@ -0,0 +1,43 @@
+let alpha = 0.05; // Tuning constant, controls how much XP gain slows down as player levels up
+let beta = 0.3; //Adds more XP when you're further from levelling up, and less XP when you're near the next level
+//defines how much the "gatekeeper" slows down the XP gain as you approach leveling up
+
+//for mocking
+let XPGain = 20;
+let currentXP = 600;
+let currentLevel = 5;
+let nextLevelXP = 800;
+let actualTimeSeconds = 20;
+let maxTimeSeconds = 30;
+let CA = 1; //Correct answer, range from (0-1)
+
+function calculateTimeReward() {
+  let RT = Math.max(0, maxTimeSeconds - actualTimeSeconds);
+  return RT / maxTimeSeconds;
+}
+
+function calculateLevelReward() {
+  let scaling = 1 + alpha * currentLevel;
+  return 1 / scaling;
+}
+
+function calculateGateKeepingComponent() {
+  let remainingXP = nextLevelXP - currentXP;
+  return beta * (remainingXP / nextLevelXP);
+}
+
+async function calculateXP() {
+  const timeReward = calculateTimeReward();
+  const levelReward = calculateLevelReward();
+  const gatekeeper = calculateGateKeepingComponent();
+
+  const XP =
+    XPGain * CA +
+    XPGain * timeReward +
+    XPGain * levelReward +
+    XPGain * gatekeeper;
+
+  return XP;
+}
+
+calculateXP().then((xp) => console.log('XP Earned:', xp));

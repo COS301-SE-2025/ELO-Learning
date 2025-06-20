bb877a5f46120b64a1f742bde83d55fe70259d06
“Crispykitty” <u22536800@tuks.co.za>
2025-06-20 15:49:06 +0200

    added input-questions page route for math input questions

---
+++ frontend/src/app/question-templates/input-questions/page.jsx
@@ -0,0 +1,21 @@
+import MathKeyboardWrapper from '@/app/ui/math-keyboard/client-wrapper';
+import { getQuestionsByType } from '@/utils/api'; // NEW API
+
+export default async function Page() {
+  const result = await getQuestionsByType('Math Input'); // Get only Math Input
+  
+  if (result.success) {
+    console.log('Math Input questions fetched successfully');
+  } else {
+    console.error('Error:', result.error);
+    return <div>Error loading input questions</div>;
+  }
+  
+  const questions = result.data;
+
+  return (
+    <div className="full-screen w-full h-full flex flex-col justify-between">
+      <MathKeyboardWrapper questions={questions} />
+    </div>
+  );
+}

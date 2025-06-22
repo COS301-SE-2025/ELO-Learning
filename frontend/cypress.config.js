9004004d24c4778f3273a01094b4a5bf71b09488
“Crispykitty” <u22536800@tuks.co.za>
2025-06-23 01:26:00 +0200

    Update Cypress configuration to include unit test patterns and set base URL for e2e tests - must be reviewed

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -6,9 +6,16 @@ export default defineConfig({
       framework: 'next',
       bundler: 'webpack',
     },
+    specPattern: 'cypress/unit/**/*.test.cy.{js,jsx}', // Add unit tests
   },
 
   e2e: {
+    baseUrl: 'http://localhost:8080',
+    specPattern: [
+      'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+      'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+      'cypress/unit/**/*.test.cy.{js,jsx,ts,tsx}',
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

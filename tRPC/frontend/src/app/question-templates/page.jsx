fd6787ff7632834a662e17dd182a2f2c123512b0
Saskia Steyn <u17267162@tuks.co.za>
2025-07-03 19:49:30 +0200

    deleted duplicate frontend

--- tRPC/frontend/src/app/question-templates/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <p>question templates will go here</p>
-    </div>
-  );
-}
--- tRPC/frontend/src/app/ui/lives.jsx
+++
@@ -1,9 +0,0 @@
-import { Heart } from 'lucide-react';
-export default function Lives({ numberOfLives }) {
-  return (
-    <div className="flex flex-row items-center gap-2">
-      <Heart size={24} fill="#FF6E99" stroke="#FF6E99" />
-      <p>{numberOfLives}</p>
-    </div>
-  );
-}

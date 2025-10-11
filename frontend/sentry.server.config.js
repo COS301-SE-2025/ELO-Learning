11708cb728d45076deb7acae2272bc54bebcece8
brogrammer012 <nigelmofati@gmail.com>
2025-10-11 23:50:05 +0200

    adding sentry to the repo for devOps

---
+++ frontend/sentry.server.config.js
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://f7c8bd836b54c56f38a4f1c6802a8bd8@o4510172948070400.ingest.us.sentry.io/4510172954689536',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
